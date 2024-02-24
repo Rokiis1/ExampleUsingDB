158be3502d2ab7b6d94f581fb1982cb59338bd3f
Rokiis1 <dailyuseracc1@gmail.com>
2024-02-24 15:35:54 +0200

    Updates

--- server/server.mjs
+++ server/server.mjs
@@ -1,11 +1,8 @@
-// Importing express module
 import express from 'express';
 
-// Creating an express application
 const app = express();
 
-// Have the app listen on a specific port
-const port = 3000;
-app.listen(port, () => {
-	console.log(`Server is running and listening on port ${port}`);
+// Start the server
+app.listen(3000, () => {
+	console.log('Server is listening on port 3000');
 });
