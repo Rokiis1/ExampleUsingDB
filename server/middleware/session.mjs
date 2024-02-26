0ef7bd1ba01350bfcd8d58bfa8f19387406be105
Rokiis1 <dailyuseracc1@gmail.com>
2024-02-26 18:34:32 +0200

    comments

--- server/middleware/session.mjs
+++ server/middleware/session.mjs
@@ -1,8 +1,16 @@
 import session from 'express-session';
 
 export default session({
+	// what is secret?
+	// Secret is a string that is used to sign the session ID cookie.
 	secret: 'mySecret',
+	// what is resave?
+	// Resave is a boolean that specifies whether the session should be saved if it hasn't been modified.
 	resave: false,
+	// what is saveUninitialized?
+	// SaveUninitialized is a boolean that specifies whether a new but unmodified session should be saved.
 	saveUninitialized: true,
+	// what is cookie?
+	// Cookie is an object that specifies the session cookie.
 	cookie: { secure: false }
 });
